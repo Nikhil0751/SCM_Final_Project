d7de958cc5e55eb966906317ed200fc2c6ef54d4
Nikhil-Goyal <nikhilgoyal1817@gmail.com>
2024-05-02 14:50:20 +0530

    First version of our google replica project

---
+++ pics/module.py
@@ -0,0 +1,11 @@
+def fun(){
+    if n>1: 
+        for i in rabmge(2,int(n/2)+1)
+            ifn%==0: 
+                print(n,"is not a prime number")
+                break 
+                else:
+                    print(n,"is not a prime number")
+    else:
+        print(n,"is not a prime number")
+    }                    
